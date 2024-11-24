dd796a56c2fbde5530541cc5a9af682dd76694ef
Mrpoponep <hotu040304@gmail.com>
2024-11-24 22:35:41 +0700

    upload

--- src/DigitalVideoDisc.java
+++ src/DigitalVideoDisc.java
@@ -30,6 +30,10 @@ public class DigitalVideoDisc {
         this.cost = cost;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getTitle() {
         return title;
     }
