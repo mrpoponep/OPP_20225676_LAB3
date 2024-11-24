f80454396ce090a467617052f6cfb4ca271cb942
Mrpoponep <hotu040304@gmail.com>
2024-11-25 02:48:37 +0700

    add packages

---
+++ OtherProject/hust/soict/dsai/lab01/FirstDialog.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+    public static void main(String[] args) {
+        String ten = JOptionPane.showInputDialog("Nhập tên của bạn:");
+        JOptionPane.showMessageDialog(null, "Chào mừng bạn, " + ten + "!", "Chào mừng", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
+}
